9bf466d0ee8a63ce6517164f28d3126fd40d22a3
RichardBuah <rbbuah@gmail.com>
2024-10-05 16:10:29 -0600

    Program Structure made no errors.

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+    private void HideRandomWords(int numberToHide)
+    {
+
+    }
+    public string GetDisplayText()
+    {
+        return "";
+    }
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    private Word(string text)
+    {
+
+    }
+    public void Hide()
+    {
+
+    }
+    public void Show()
+    {
+
+    }
+    public bool IsHidden()
+    {
+        return false;
+    }
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
